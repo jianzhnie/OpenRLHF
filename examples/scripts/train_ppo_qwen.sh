85d8438098f7e5a242be1b8fbafd7e8c05154d35
JianZhengNie <jianzhnie@126.com>
2025-03-03 15:32:21 +0800

    Update OpenRLHF

--- examples/scripts/train_ppo_qwen.sh
+++ examples/scripts/train_ppo_qwen.sh
@@ -4,24 +4,27 @@ read -r -d '' training_commands <<EOF
 openrlhf.cli.train_ppo \
    --pretrain /home/robin/hf_hub/models/Qwen/Qwen1.5-0.5B \
    --critic_pretrain /home/robin/hf_hub/models/Qwen/Qwen1.5-0.5B \
-   --reward_func_names accuracy format \
+   --reward_func_names accuracy format reasoning_steps cosine \
    --save_path ./work_dir/qwen2.5-1.5b-rlhf \
-   --save_steps 1000 \
+   --ckpt_path ./work_dir/qwen2.5-1.5b-rlhf/ckpt \
+   --save_hf_ckpt \
+   --max_ckpt_num 100 \
+   --save_steps 10 \
    --logging_steps 1 \
    --eval_steps -1 \
    --micro_train_batch_size 2 \
    --train_batch_size 128 \
    --micro_rollout_batch_size 4 \
-   --rollout_batch_size 8 \
+   --rollout_batch_size 1024 \
    --max_epochs 1 \
-   --prompt_max_len 256 \
-   --generate_max_len 256 \
+   --prompt_max_len 1024 \
+   --generate_max_len 1024 \
    --zero_stage 2 \
    --bf16 \
    --actor_learning_rate 5e-7 \
    --critic_learning_rate 9e-6 \
    --init_kl_coef 0.01 \
-   --prompt_data AI-MO/NuminaMath-TIR \
+   --prompt_data /home/robin/hf_hub/datasets/text_data/AI-MO/NuminaMath-TIR \
    --input_key problem \
    --label_key solution \
    --apply_chat_template \
@@ -30,7 +33,7 @@ openrlhf.cli.train_ppo \
    --adam_offload \
    --load_checkpoint \
    --gradient_checkpointing \
-   --use_tensorboard  ./work_dir/tb_log
+   --use_tensorboard  ./work_dir/qwen2.5-1.5b-rlhf/tb_log
 EOF
 
     # --packing_samples
