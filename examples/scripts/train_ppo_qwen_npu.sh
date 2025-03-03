2309eb30b360bebebc2ef5c031166ded6fea274d
JianZhengNie <jianzhnie@126.com>
2025-03-03 10:01:45 +0800

    Update OpenRLHF

---
+++ examples/scripts/train_ppo_qwen_npu.sh
@@ -0,0 +1,46 @@
+set -x
+
+read -r -d '' training_commands <<EOF
+openrlhf.cli.train_ppo \
+   --pretrain /home/ma-user/work/models/Qwen/Qwen2.5-1.5B \
+   --critic_pretrain /home/ma-user/work/models/Qwen/Qwen2.5-1.5B \
+   --reward_func_names accuracy format reasoning_steps cosine \
+   --save_path ./work_dir/qwen2.5-1.5b-rlhf \
+   --ckpt_path ./work_dir/qwen2.5-1.5b-rlhf/ckpt \
+   --save_hf_ckpt \
+   --max_ckpt_num 100 \
+   --save_steps 1000 \
+   --logging_steps 1 \
+   --eval_steps -1 \
+   --micro_train_batch_size 2 \
+   --train_batch_size 128 \
+   --micro_rollout_batch_size 4 \
+   --rollout_batch_size 8 \
+   --max_epochs 1 \
+   --prompt_max_len 256 \
+   --generate_max_len 256 \
+   --zero_stage 2 \
+   --bf16 \
+   --actor_learning_rate 5e-7 \
+   --critic_learning_rate 9e-6 \
+   --init_kl_coef 0.01 \
+   --use_kl_estimator_k3 \
+   --prompt_data /home/ma-user/work/datasets/AI-MO/NuminaMath-TIR \
+   --input_key problem \
+   --label_key solution \
+   --apply_chat_template \
+   --max_samples 100000 \
+   --normalize_reward \
+   --adam_offload \
+   --load_checkpoint \
+   --gradient_checkpointing \
+   --use_tensorboard  ./work_dir/qwen2.5-1.5b-rlhf/tb_log
+EOF
+
+    # --packing_samples
+    # --use_wandb [WANDB_TOKENS] or True (use wandb login command)
+    # --remote_rm_url http://localhost:5000/get_reward
+
+if [[ ${1} != "slurm" ]]; then
+    deepspeed --module $training_commands
+fi
---
+++ run.sh
@@ -0,0 +1,3 @@
+# source /home/ma-user/work/miniconda3/bin/activate
+# conda activate openrlhf
+nohup sh examples/scripts/train_ppo_qwen.sh > train_ppo_qwen.log 2>&1 &
